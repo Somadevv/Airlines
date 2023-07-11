ecd3c9322cc0e64d0167b8b9141872f5cec45fd6
Josh Howarth <josh@insitesdigital.com>
2023-07-11 02:50:50 +0100

    Add flight search component

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@
 import { Header } from "@/components/organisms/Header/Header";
 import { Banner } from "@/components/organisms/Banner/Banner";
 import homepageImage from "../public/assets/images/background-image.jpg";
-import trustpilotStars from "../public/assets/images/trustpilot.png";
 import { TrustPilot } from "@/components/atoms/TrustPilot/TrustPilot";
+import { FlightSearch } from "@/components/molecules/FlightSearch/FlightSearch";
+
 import trustpilotData from "../data/trustpilot.json";
 
 const Home = () => {
@@ -16,6 +17,7 @@ const Home = () => {
         body="Buy your flights now and pay later"
       >
         <TrustPilot data={trustpilotData} />
+        <FlightSearch />
       </Banner>
     </main>
   );
---
+++ components/molecules/FlightSearch/FlightSearch.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const FlightSearch = () => {
+  return <div>FlightSearch</div>;
+};
